1e09228fadfdcc359d73570b6a6de9a98fca0613
sharkk2 <129411155+sharkk2@users.noreply.github.com>
2025-09-14 12:15:15 +0300

    Update on_guild_join.py

--- src/core/events/on_guild_join.py
+++ src/core/events/on_guild_join.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from core.functions.initilize_guild import initilize_guild
- 
+import config 
      
 class onguildjoin(commands.Cog):
     def __init__(self, bot):
@@ -9,10 +9,12 @@ class onguildjoin(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild: discord.Guild):
-        await initilize_guild(guild, self.bot)
+        if config.connect_db:
+          await initilize_guild(guild, self.bot)
                  
          
 async def setup(bot):
     global Bot
     Bot = bot
+
     await bot.add_cog(onguildjoin(bot))         
